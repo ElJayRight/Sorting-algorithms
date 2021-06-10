0c26bfe0b562b16b9d5f01b7a8389965c23910bd
ElJayRight <85490118+ElJayRight@users.noreply.github.com>
2021-06-10 20:22:25 +1000

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,17 @@
+def selection(A):
+    for i in range(len(A)):
+        print(A)
+        # Find the minimum element in remaining
+        # unsorted array
+        min_idx = i
+        for j in range(i+1, len(A)):
+            if A[min_idx] > A[j]:
+                min_idx = j
+                
+        # Swap the found minimum element with
+        # the first element       
+        A[i], A[min_idx] = A[min_idx], A[i]
+arr = [64, 34, 25, 12, 22, 11, 90, 38, 86, 56]
+
+if __name__ =='__main__':
+    selection(arr)
